3b3879f39c749fec3f95e2ac3cf01141087eb026
Nelson Vieira <nelson0.vieira@gmail.com>
2023-03-29 20:12:30 +0100

    Updated index
    Signed-off-by: Nelson Vieira <nelson0.vieira@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 <h2 class="text-2xl font-bold">Welcome to my website</h2>
 <p class="text-xl">Coming soon&#8482;</p>
 <br>
+<br>
 <a href="https://github.com/nelson-vieira">My GitHub</a>
 
 <style lang="postcss">
