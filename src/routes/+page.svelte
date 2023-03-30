fa89fb0e93135ec2f262601ffb362f76cec7024f
Nelson Vieira <nelson0.vieira@gmail.com>
2023-03-30 11:30:53 +0100

    Refactored codebase: switched tabs for spaces Updated index frontend
    Signed-off-by: Nelson Vieira <nelson0.vieira@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,7 @@
 <br>
 <p class="text-xl">Coming soon&#8482;</p>
 <br>
-<br>
-<a href="https://github.com/nelson-vieira">My GitHub</a>
+<a href="https://github.com/nelson-vieira" class="text-blue-500 underline visited:text-purple-500">My GitHub</a>
 
 <style lang="postcss">
     :global(html) {
