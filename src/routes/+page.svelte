2c332f373dfc63f60eacce295e71bcbbe4df6ea3
Nelson Vieira <nelson0.vieira@gmail.com>
2023-04-10 16:24:09 +0100

    Updated index frontend
    Signed-off-by: Nelson Vieira <nelson0.vieira@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,31 @@
-<h1 class="text-3xl font-bold">Hi there, I'm Nelson Vieira</h1>
-<br>
-<h2 class="text-2xl font-bold">Welcome to my website</h2>
-<br>
-<p class="text-xl">Coming soon&#8482;</p>
-<br>
-<a href="https://github.com/nelson-vieira" class="text-blue-500 underline visited:text-purple-500">My GitHub</a>
+<div id="titles" class="m-0 p-0 border-0 text-[100%] align-baseline">
+    <h1 class="m-0 p-0 border-0 text-[4.5vw] align-baseline
+    font-bold
+    vsm:text-3xl">
+        Hi there, I'm Nelson Vieira
+    </h1>
+    <br>
+    <h2 class="m-0 p-0 border-0 text-[4.5vw] align-baseline
+    font-bold
+    vsm:text-2xl">
+        Welcome to my website
+    </h2>
+    <br>
+</div>
+<div id="content" class="m-0 p-0 border-0 text-[100%] align-baseline">
+    <p class="m-0 p-0 border-0 text-[4.5vw] align-baseline
+    vsm:text-xl">
+        Coming soon&#8482;
+    </p>
+    <br>
+    <a href="https://github.com/nelson-vieira" class="m-0 p-0 border-0 text-[4.5vw] align-baseline
+    text-blue-500 underline
+    vsm:text-sm
+    sm:!text-base
+    visited:text-purple-500">
+        My GitHub
+    </a>
+</div>
 
 <style lang="postcss">
     :global(html) {
