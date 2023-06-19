b83f5db52560242f145bfca935a2bced95f23413
Nelson Vieira <hello@nelsonvieira.me>
2023-06-19 13:14:56 +0100

    Add background image to index
    Signed-off-by: Nelson Vieira <hello@nelsonvieira.me>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,50 @@
-<div id="titles" class="m-0 p-0 border-0 text-[100%] align-baseline">
-    <h1 class="m-0 p-0 border-0 text-[4.5vw] align-baseline
-    font-bold
-    vsm:text-3xl">
-        Hi there, I'm Nelson Vieira
-    </h1>
-    <br>
-    <h2 class="m-0 p-0 border-0 text-[4.5vw] align-baseline
-    font-bold
-    vsm:text-2xl">
-        Welcome to my website
-    </h2>
-    <br>
-</div>
-<div id="content" class="m-0 p-0 border-0 text-[100%] align-baseline">
-    <p class="m-0 p-0 border-0 text-[4.5vw] align-baseline
-    vsm:text-xl">
-        Coming soon&#8482;
-    </p>
-    <br>
-    <a href="https://github.com/nelson-vieira" class="m-0 p-0 border-0 text-[4.5vw] align-baseline
-    text-blue-500 underline
-    vsm:text-sm
-    sm:!text-base
-    visited:text-purple-500">
-        My GitHub
-    </a>
-</div>
-
+<script>
+    import img from "$lib/assets/images/purple_and_yellow_abstract_painting.jpg";
+</script>
 <style lang="postcss">
     :global(html) {
         background-color: theme(colors.gray.100);
     }
 </style>
+<div class="m-0 p-0 border-0 text-[100%] align-baseline
+relative w-screen h-screen" role="img" aria-label="Purple and yellow abstract painting"
+style="background-image: url({img}); background-size: cover; background-repeat: no-repeat;">
+    <div class="m-0 p-0 border-0 text-[100%] align-baseline
+    flex flex-col flex-nowrap justify-center items-center w-screen h-screen text-center">
+        <div id="titles" class="m-0 p-0 border-0 text-[100%] align-baseline">
+            <h1 class="m-0 p-0 border-0 text-[4.5vw] align-baseline
+            font-bold text-white
+            vsm:text-3xl">
+                Hi there, I'm Nelson Vieira
+            </h1>
+            <br>
+            <h2 class="m-0 p-0 border-0 text-[4.5vw] align-baseline
+            font-bold text-white
+            vsm:text-2xl">
+                Welcome to my website
+            </h2>
+            <br>
+        </div>
+        <div id="content" class="m-0 p-0 border-0 text-[100%] align-baseline">
+            <p class="m-0 p-0 border-0 text-[4.5vw] align-baseline
+            text-white
+            vsm:text-xl">
+                Coming soon&#8482;, for sure in the next 30 years.
+            </p>
+            <br>
+            <a href="https://github.com/nelson-vieira" class="m-0 p-0 border-0 text-[4.5vw] align-baseline
+            text-blue-500 underline
+            vsm:text-sm
+            sm:!text-base
+            visited:text-blue-500">
+                GitHub
+            </a>
+        </div>
+    </div>
+    <span class="m-0 p-0 border-0 text-[100%] align-baseline
+    hidden absolute bottom-0 text-white">
+        Background photo by<a href="https://unsplash.com/@lazycreekimages?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" class="m-0 p-0 border-0 text-[100%] align-baseline
+        text-blue-500 underline">Michael Dziedzic</a> on <a href="https://unsplash.com/pt-br/fotografias/0W4XLGITrHg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" class="m-0 p-0 border-0 text-[100%] align-baseline
+        text-blue-500 underline">Unsplash</a>
+    </span>
+</div>
