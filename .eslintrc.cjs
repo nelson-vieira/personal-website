d2f675a6873abbdd1c8044494524cd9d4a9e2fe7
Nelson Vieira <hello@nelsonvieira.me>
2023-06-23 11:54:37 +0100

    Change license to AGPL-3.0
    Signed-off-by: Nelson Vieira <hello@nelsonvieira.me>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,9 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: AGPL-3.0-or-later
 //
 // Copyright (c) 2023 Nelson Vieira
 //
 // @author Nelson Vieira <hello@nelsonvieira.me>
-// @license GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>
+// @license AGPL-3.0 <https://www.gnu.org/licenses/agpl-3.0.txt>
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
