719102cc854180c83c5b2e74236f4dd212cd8b33
Nelson Vieira <hello@nelsonvieira.me>
2024-06-05 13:00:56 +0100

    Add 'Perfect DOS VGA 437' font
    Signed-off-by: Nelson Vieira <hello@nelsonvieira.me>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,93 +29,96 @@ SPDX-FileCopyrightText: Copyright 2023-2024 Nelson Vieira
     }
 </style>
 <div class="m-0 p-0 border-0 text-[100%] align-baseline
-absolute w-full h-full" role="img" aria-label="Purple and yellow abstract painting"
-style="background-image: url({img}); background-size: cover; background-repeat: no-repeat;"></div>
-<div id="content" class="m-0 p-0 border-0 text-[100%] align-baseline
-relative min-h-screen flex flex-col justify-center items-center">
-    <div class="mt-auto p-0 border-0 text-[100%] align-baseline
-    flex flex-col flex-nowrap justify-center items-center text-center">
-        <div id="titles" class="m-0 p-0 border-0 text-[100%] align-baseline">
-            <h1 class="m-0 p-0 border-0 text-[4.5vw] align-baseline
-            font-bold text-zinc-100
-            dark:text-zinc-100
-            hover:text-green-500 hover:bg-gray-800/20
-            vsm:text-3xl">
-                Hi there, I'm Nelson Vieira
-            </h1>
-            <br>
-            <h2 class="m-0 p-0 border-0 text-[4.5vw] align-baseline
-            font-bold text-zinc-100
-            dark:text-zinc-100
-            hover:text-red-600 hover:bg-gray-800/20
-            vsm:text-2xl">
-                Welcome to my domain
-            </h2>
-            <br>
+font-['Perfect_DOS_VGA_437']">
+    <div class="m-0 p-0 border-0 text-[100%] align-baseline
+    absolute w-full h-full" role="img" aria-label="Purple and yellow abstract painting"
+    style="background-image: url({img}); background-size: cover; background-repeat: no-repeat;"></div>
+    <div id="content" class="m-0 p-0 border-0 text-[100%] align-baseline
+    relative min-h-screen flex flex-col justify-center items-center">
+        <div class="mt-auto p-0 border-0 text-[100%] align-baseline
+        flex flex-col flex-nowrap justify-center items-center text-center">
+            <div id="titles" class="m-0 p-0 border-0 text-[100%] align-baseline">
+                <h1 class="m-0 p-0 border-0 text-[4.5vw] align-baseline
+                font-bold text-zinc-100
+                dark:text-zinc-100
+                hover:text-green-500 hover:bg-gray-800/20
+                vsm:text-3xl">
+                    Hi there, I'm Nelson Vieira
+                </h1>
+                <br>
+                <h2 class="m-0 p-0 border-0 text-[4.5vw] align-baseline
+                font-bold text-zinc-100
+                dark:text-zinc-100
+                hover:text-red-600 hover:bg-gray-800/20
+                vsm:text-2xl">
+                    Welcome to my domain
+                </h2>
+                <br>
+            </div>
+            <div id="text" class="m-0 p-0 border-0 text-[100%] align-baseline">
+                <p class="m-0 p-0 border-0 text-[4.5vw] align-baseline
+                text-zinc-100
+                dark:text-zinc-100
+                hover:text-orange-300 hover:bg-gray-800/20
+                vsm:text-xl">
+                    Coming soon in the next 30 years...
+                </p>
+                <br>
+                <a href="https://github.com/nelson-vieira" class="m-0 p-0 border-0 text-[5vw] align-baseline
+                text-zinc-100 !leading-none
+                vsm:text-base
+                sm:!text-3xl
+                hover:text-zinc-800
+                visited:text-zinc-100" title="GitHub">
+                    <i class="lni lni-github-original"></i>
+                </a>
+            </div>
         </div>
-        <div id="text" class="m-0 p-0 border-0 text-[100%] align-baseline">
-            <p class="m-0 p-0 border-0 text-[4.5vw] align-baseline
-            text-zinc-100
+        <div class="mt-auto p-0 border-0 text-[100%] align-baseline
+        bg-transparent">
+            <p class="m-0 p-0 border-0 text-[4vw] align-baseline
+            text-zinc-100 text-center
             dark:text-zinc-100
-            hover:text-orange-300 hover:bg-gray-800/20
-            vsm:text-xl">
-                Coming soon&#8482; in the next 30 years...
+            vsm:text-base">
+                Background photo by <a href="https://unsplash.com/@lazycreekimages?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" class="m-0 p-0 border-0 text-[100%] align-baseline
+                text-blue-400
+                hover:underline
+                focus:text-blue-700
+                active:no-underline active:text-blue-950
+                visited:text-blue-400">Michael Dziedzic</a> on <a href="https://unsplash.com/pt-br/fotografias/0W4XLGITrHg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" class="m-0 p-0 border-0 text-[100%] align-baseline
+                text-blue-400
+                hover:underline
+                focus:text-blue-700
+                active:no-underline active:text-blue-950
+                visited:text-blue-400">Unsplash</a>.
             </p>
-            <br>
-            <a href="https://github.com/nelson-vieira" class="m-0 p-0 border-0 text-[5vw] align-baseline
-            text-zinc-100 !leading-none
-            vsm:text-base
-            sm:!text-3xl
-            hover:text-zinc-800
-            visited:text-zinc-100" title="GitHub">
-                <i class="lni lni-github-original"></i>
-            </a>
+            <p class="m-0 p-0 border-0 text-[4vw] align-baseline
+            text-zinc-100 text-center
+            dark:text-zinc-100
+            vsm:text-base">
+                Page content licensed under a
+                <a href="https://creativecommons.org/licenses/by/4.0/legalcode.txt" class="m-0 p-0 border-0 text-[100%] align-baseline
+                text-blue-400
+                hover:underline
+                focus:text-blue-700
+                active:no-underline active:text-blue-950
+                visited:text-blue-400">
+                    CC-BY-4.0 license</a>.
+                <a href="https://github.com/nelson-vieira/personal-website" class="m-0 p-0 border-0 text-[100%] align-baseline
+                text-blue-400
+                hover:underline
+                focus:text-blue-700
+                active:no-underline active:text-blue-950
+                visited:text-blue-400">
+                    Source code</a> licensed under a
+                <a href="https://www.gnu.org/licenses/agpl-3.0.txt" class="m-0 p-0 border-0 text-[100%] align-baseline
+                text-blue-400
+                hover:underline
+                focus:text-blue-700
+                active:no-underline active:text-blue-950
+                visited:text-blue-400">
+                    AGPL-3.0-or-later license</a>.
+            </p>
         </div>
-    </div>
-    <div class="mt-auto p-0 border-0 text-[100%] align-baseline
-    bg-transparent">
-        <p class="m-0 p-0 border-0 text-[4vw] align-baseline
-        text-zinc-100 text-center
-        dark:text-zinc-100
-        vsm:text-base">
-            Background photo by <a href="https://unsplash.com/@lazycreekimages?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" class="m-0 p-0 border-0 text-[100%] align-baseline
-            text-blue-400
-            hover:underline
-            focus:text-blue-700
-            active:no-underline active:text-blue-950
-            visited:text-blue-400">Michael Dziedzic</a> on <a href="https://unsplash.com/pt-br/fotografias/0W4XLGITrHg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" class="m-0 p-0 border-0 text-[100%] align-baseline
-            text-blue-400
-            hover:underline
-            focus:text-blue-700
-            active:no-underline active:text-blue-950
-            visited:text-blue-400">Unsplash</a>.
-        </p>
-        <p class="m-0 p-0 border-0 text-[4vw] align-baseline
-        text-zinc-100 text-center
-        dark:text-zinc-100
-        vsm:text-base">
-            Page content licensed under a
-            <a href="https://creativecommons.org/licenses/by/4.0/legalcode.txt" class="m-0 p-0 border-0 text-[100%] align-baseline
-            text-blue-400
-            hover:underline
-            focus:text-blue-700
-            active:no-underline active:text-blue-950
-            visited:text-blue-400">
-                CC-BY-4.0 license</a>.
-            <a href="https://github.com/nelson-vieira/personal-website" class="m-0 p-0 border-0 text-[100%] align-baseline
-            text-blue-400
-            hover:underline
-            focus:text-blue-700
-            active:no-underline active:text-blue-950
-            visited:text-blue-400">
-                Source code</a> licensed under a
-            <a href="https://www.gnu.org/licenses/agpl-3.0.txt" class="m-0 p-0 border-0 text-[100%] align-baseline
-            text-blue-400
-            hover:underline
-            focus:text-blue-700
-            active:no-underline active:text-blue-950
-            visited:text-blue-400">
-                AGPL-3.0-or-later license</a>.
-        </p>
     </div>
 </div>
